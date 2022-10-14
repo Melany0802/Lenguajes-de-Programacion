0e478847312524fe30aab5774757153f1c4658bb
Melany Jiménez <91802678+Melany0802@users.noreply.github.com>
2022-10-14 15:09:26 -0600

    Delete Programación Lógico directory

--- Programación Lógico/eliminarElemento/eliminarElemento.pl
+++
@@ -1,7 +0,0 @@
-eliminarElemento(_, [], []).
-
-eliminarElemento(Y, [Y|Xs], Zs):-
-          eliminarElemento(Y, Xs, Zs), !.
-
-eliminarElemento(X, [Y|Xs], [Y|Zs]):-
-          eliminarElemento(X, Xs, Zs).
